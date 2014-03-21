36dccf053a10b4531cb207aa1dd8f605d0bac808
Brendan Thompson <brendan.thompson@ezidebit.com.au>
2014-03-21 10:23:47 +1000

    Initial Commit

---
+++ rDelete/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace rDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = new DirectoryInfo(Environment.CurrentDirectory);
+            
+            RemoveReadOnlyAttributesAndDelete(currentDirectory.FullName);
+        }
+
+        private static void RemoveReadOnlyAttributesAndDelete(string dir)
+        {
+            var d = new DirectoryInfo(dir);
+
+            foreach (var file in d.GetFiles())
+            {
+                File.SetAttributes(file.FullName, File.GetAttributes(file.FullName) & ~FileAttributes.ReadOnly);
+                file.Refresh();
+                file.Delete();
+
+                file.Delete();
+            }
+
+            foreach (var directoryInfo in d.GetDirectories())
+            {
+                directoryInfo.Attributes = FileAttributes.Normal;
+                File.SetAttributes(directoryInfo.FullName, File.GetAttributes(directoryInfo.FullName) & ~FileAttributes.ReadOnly);
+                directoryInfo.Refresh();
+
+                RemoveReadOnlyAttributesAndDelete(directoryInfo.FullName);
+
+                directoryInfo.Delete(true);
+            }
+        }
+    }
+}
---
+++ rDelete/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rDelete")]
+[assembly: AssemblyDescription("Recursively deletes files and directories at input.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BT Systems")]
+[assembly: AssemblyProduct("rDelete")]
+[assembly: AssemblyCopyright("Copyright BT Systems © 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d93aae24-2656-403c-ae0f-e0bd657ee9c9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.2.1")]
+[assembly: AssemblyInformationalVersionAttribute("0.2.1-beta")]
